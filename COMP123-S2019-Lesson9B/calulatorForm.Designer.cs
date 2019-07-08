c93edcef7b3c445f195767d6fefe3d7dffd4eeeb
sean <mkwak@my.centennialcollege.ca>
2019-07-08 13:52:49 -0400

    created base project

---
+++ COMP123-S2019-Lesson9B/calulatorForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace COMP123_S2019_Lesson9B
+{
+    partial class CalulatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CalculatorButtontableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // CalculatorButtontableLayoutPanel
+            // 
+            this.CalculatorButtontableLayoutPanel.ColumnCount = 3;
+            this.CalculatorButtontableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.CalculatorButtontableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.CalculatorButtontableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.CalculatorButtontableLayoutPanel.Location = new System.Drawing.Point(12, 64);
+            this.CalculatorButtontableLayoutPanel.Name = "CalculatorButtontableLayoutPanel";
+            this.CalculatorButtontableLayoutPanel.RowCount = 4;
+            this.CalculatorButtontableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.CalculatorButtontableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.CalculatorButtontableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.CalculatorButtontableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.CalculatorButtontableLayoutPanel.Size = new System.Drawing.Size(760, 315);
+            this.CalculatorButtontableLayoutPanel.TabIndex = 0;
+            // 
+            // CalulatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.CalculatorButtontableLayoutPanel);
+            this.Name = "CalulatorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel CalculatorButtontableLayoutPanel;
+    }
+}
+
